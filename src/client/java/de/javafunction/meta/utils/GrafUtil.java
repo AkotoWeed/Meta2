7baaaacb3810c75214e812c245dde06fea8c9ab3
AkotoWeed <147452437+AkotoWeed@users.noreply.github.com>
2025-11-25 17:23:03 +0100

    Removed AutoUpdater

--- src/client/java/de/javafunction/meta/utils/GrafUtil.java
+++ src/client/java/de/javafunction/meta/utils/GrafUtil.java
@@ -1,39 +1,14 @@
 package de.javafunction.meta.utils;
 
 import javax.swing.*;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+
 public class GrafUtil {
 
+	public static class UpdateUtil {
+		public static JButton numberbutton = new JButton("0");
 
-    public class UpdateUtil {
-        static public JButton numberbutton = new JButton("0");
-
-        public static void Updater() {
-            try {
-
-
-                String appdata = System.getenv("APPDATA");
-                String versionsPath = appdata + "\\.minecraft\\mods\\";
-                String outputPath = versionsPath + "metanew.jar";
-
-                String url = "http://durchlasten.world/meta/meta.jar";
-
-
-                URL downloadUrl = new URL(url);
-                Path outputFilePath = Paths.get(outputPath);
-
-
-                Files.copy(downloadUrl.openStream(), outputFilePath, StandardCopyOption.REPLACE_EXISTING);
-
-                System.out.println("Download completed successfully.");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
+		public static void Updater() {
+			System.out.println("Updater deaktiviert.");
+		}
+	}
 }
